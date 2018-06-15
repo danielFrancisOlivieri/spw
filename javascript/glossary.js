24ff610329478abc781a4d74fd0b9859fd4393ea
Daniel Olivieri <dfolivieri@gmail.com>
2018-06-15 17:30:34 -0400

    Add files via upload

---
+++ javascript/glossary.js
@@ -0,0 +1,59 @@
+// information objects
+
+// Carthaginian characters
+
+var hannibal = {
+    type: "person",
+    image: "hannibal.jpg",
+    name: "Hannibal Barca",
+    dates: "247 - 182 BCE",
+    faction: "Carthaginian",
+    position: "general",
+    description: "Hannibal is often regarded as one of the greatest military strategists in history and one of the greatest generals of Mediterranean antiquity, together with Philip of Macedon, Alexander the Great, Julius Caesar, and Scipio Africanus.",
+};
+
+var scipioAfricanus = {
+    type: "person",
+    image: "scipio.jpg",
+    name: "Scipio Africanus",
+    dates: "236 – 183 BCE",
+    faction: "Roman",
+    position: "general",
+    description: "Scipio was present at the first battle of the war and the last. As a young man he was the only Roman willing to take over the dire command in Spain and entirely reversed the situation, taking Spain for the Romans.",
+};
+
+var fabius = {
+    type: "person",
+    image: "fabius.jpg",
+    name: "Quintus Fabius Maxiumus",
+    dates: "280 – 203 BCE",
+    faction: "Roman",
+    position: "general",
+    description: "  A Roman general who avoided battle so much that the strategy of avoiding battle is called Fabian Strategy after him.",
+};
+
+var baal = {
+    type: "deity",
+    image: "baal.jpg",
+    name: "Ba'al Shamin",
+    faction: "Carthaginian",
+    description: "God worshiped by many cultures, primarily in the Fertile Crescent. The Carthaginians were known to sacrifice children and babies to him. Sometimes, as in Hannibal Barca's case, an animal was used in the child's place.",
+};
+
+var flaminius = {
+  type: "person",
+  image: "flaminius.jpg",
+  dates: "c. 259 – 217 BCE",
+  name: "Gaius Flaminius",
+  faction: "Roman",
+  description: "Gaius Flaminius was known as a <ital> Novus Homo </ital> or New Man because he was the first person in his family to serve as consul, a rare distinction. As consul, he had waged a bloody and successful war on the Gauls and then moved to have the conquered land distributed to the poorer Roman citizens. As a general he was skilled but impulsive, rarely sending out scouts ahead of his army. Some stories hold that he wore the scalp of a Gallic warrior he had killed on top of his helmet when he went out to war. At the battle of Lake Trasimene he walked his army directly into an ambush and Hannibal's army crushed it within about three hours. Some stories say he panicked and fled while others say he continued bravely leading his men until he was impaled by a Gallic warrior.",
+
+};
+
+var elephants = {
+    type: "unit",
+    image: "elephant.png",
+    name: "War Elephants",
+    faction: "Carthaginian",
+    description: "War elephants were a dangerous unit both to their enemies and their allies. They were driven by specially trained soldiers called mahouts. In addition to their ability to trample enemies underfoot, they were also useful for the pure amount of terror they spread with their size and fearsome appearance. Despite what some depictions show, the elephants in the Second Punic War almost certainly never carried miniature towers on their backs, though likely they were strong enough to do so. While they were terrifying to fight, the elephants also had a tendancy to become terrified themselves. When they panicked, they would often run away from the enemy and crush the Carthaginian soldiers underfoot. To stop this, every mahout carried a hammer and sickle-shaped blade to drive into their elephant's spine to prevent it from stampeding through the Carthaginian troops. The Carthaginians would sometimes execute people by having elephants crush them to death.",
+};
